6d9f9e2adf7aad7016083d864d1f264d4a79d351
wormmy55 <67141048+wormmy55@users.noreply.github.com>
2020-06-21 12:35:42 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+let Hand = 0
+input.onGesture(Gesture.Shake, function () {
+    Hand = randint(1, 3)
+    if (Hand == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
